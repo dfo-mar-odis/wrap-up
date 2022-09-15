0223ec34011694cd9af6920bca1a3fc08c9dc2f6
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2022-09-15 14:07:51 -0300

    fix bugs found from actually running the code

--- sept16/car.py
+++ sept16/car.py
@@ -4,21 +4,21 @@ from datetime import datetime
 class Car:
     wheel_num = 5
     oil_tank_size = 7
+    oil_change_dates = []
 
     def __init__(self, car_make, year):
         self.car_make = car_make
         self.year = year
 
         self.hood_open = False
-        self.oil_change_dates = []
         self.oil_level = self.oil_tank_size
 
-    def change_oil(self):
-            self.hood_open = True
-            self.empty_oil()
-            self.add_oil(self.oil_tank_size)
-            self.hood_open = False
-            self.oil_change_dates.append(datetime.today())
+    def change_oil(self, date):
+        self.hood_open = True
+        self.empty_oil()
+        self.add_oil(self.oil_tank_size)
+        self.hood_open = False
+        self.oil_change_dates.append(date)
 
     def empty_oil(self):
         self.oil_level = 0
--- sept16/demo_script.py
+++ sept16/demo_script.py
@@ -6,7 +6,7 @@ other_car = Car("honda", 2010)
 print("Before, test car: {}".format(test_car.oil_change_dates))
 print("Before, other car: {}".format(other_car.oil_change_dates))
 
-test_car.change_oil()
+test_car.change_oil("2022-09-16")
 
 print("After, test car: {}".format(test_car.oil_change_dates))
 print("After, other car: {}".format(other_car.oil_change_dates))
